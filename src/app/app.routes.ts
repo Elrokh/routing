411e24f60e79c670a9a474ffca1b81ac6d82ab58
Elrokh <mustafaelrokh77@gmail.com>
2025-08-03 02:13:54 +0300

    init project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { About } from './component/home/about/about';
+import { Gallery } from './component/home/gallery/gallery';
+import { Home } from './component/home/home';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: Home, title: 'home page' },
+  { path: 'about', component: About, title: 'about page' },
+  { path: 'gallery', component: Gallery, title: 'gallery page' },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './component/home/home';
+import { Navbar } from './component/home/navbar/navbar';
+import { About } from './component/home/about/about';
+import { Footer } from './component/home/footer/footer';
+import { Gallery } from './component/home/gallery/gallery';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('routing');
